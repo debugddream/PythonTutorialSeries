102ee53e89c1aabf9cc6e392ff434494dc78fd48
Hammadulhaq123 <professorcoding123@gmail.com>
2023-09-28 17:39:32 +0500

    Casting and String methods

---
+++ casting&stringmethods.py
@@ -0,0 +1,45 @@
+# type casting:
+
+"""
+int = 23
+x = str(int)
+print(type(x))
+
+y = float(int)
+print(y)
+
+
+var = 1
+z = bool(var)
+print(z)
+"""
+
+# String Methods:
+
+str1 = "    This is hammad, and my course is related to python.      "
+str2 = "Hammad"
+
+# print(str1 + str2)
+
+# fstring:
+# print(f"This is {str2}")
+
+# String array/list:
+# print(str1[6])
+
+
+# length of string
+# print(len(str1))
+
+# slicing :
+# print(str1[:4])
+
+# in or not in:
+
+print("course" not in str1)
+
+print(str1.upper())
+print(str1.lower())
+print(str1.strip())
+print(str1.replace("hammad", "raza"))
+print(str1.split(","))
