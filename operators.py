0f87e894f0a4b29356e553e26298a76416034d5f
Hammadulhaq123 <professorcoding123@gmail.com>
2023-09-28 23:56:21 +0500

    operators in python

---
+++ operators.py
@@ -0,0 +1,55 @@
+# Arithmetic Operators
+
+"""
+var1 = 123
+var2 = 23
+
+print(var1 + var2)
+print(var1 - var2)
+print(var1 * var2)
+print(var1 / var2)
+print(var1 % var2)
+"""
+
+# Assignment Operators
+"""
+var3 = 12
+var3 += 1
+print(var3)
+var3 -= 2
+print(var3)
+var3 *= 2
+print(var3)
+
+var3 /= 2
+print(var3)
+"""
+
+# Comparision Operators
+
+"""
+x = 12
+y = 45
+print(x == y)
+print(x != y)
+print(x >= y)
+print(x <= y)
+print(x > y)
+print(x < y)
+"""
+
+# Logical Operators
+
+"""
+x = 123
+y = 256
+
+if ((x > y) and (x < y)):
+    print("Both Condition satisifed")
+
+if ((x > y) or (x < y)):
+    print("One Condition satisifed")
+"""
+
+# Precedence:
+print((3+5) - (5*2))
